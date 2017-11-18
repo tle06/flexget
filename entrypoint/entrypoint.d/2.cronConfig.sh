6ec0a47e7e5402905d51d1debc25ecb1fbe81f30
Thomas ltp <tle@tlnk.fr>
2017-11-18 18:35:55 +0100

    fix cron

--- entrypoint/entrypoint.d/2.cronConfig.sh
+++ entrypoint/entrypoint.d/2.cronConfig.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-crontab -l > /tmp/mycron
+touch /tmp/mycron
 echo "@hourly /usr/local/bin/flexget -c /config/config.yml --cron execute" >> /tmp/mycron
 echo "@reboot /usr/local/bin/flexget -c /config/config.yml daemon start -d" >> /tmp/mycron
 crontab /tmp/mycron
